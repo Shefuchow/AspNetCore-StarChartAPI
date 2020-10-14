2f1464a7da3d27e0dcea9cd61c7402e45f5c6d4c
shefuchow <mschowdh@buffelo.edu>
2020-10-13 20:17:53 -0400

    second

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id;
+        [Required] public string name;
+        public int? OrbitedObjectId;
+        [NotMapped] public List<CelestialObject> Satellites;
+
+        public CelestialObject()
+        {
+        
+        }
+    }
+}
